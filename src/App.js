334c204208fbe1f75cc2f32129f4af5e7d8ad578
mohamed bekrine <mohamed.bekrine.mb@gmail.com>
2022-10-15 22:31:54 +0100

    connection-firebase

--- src/App.js
+++ src/App.js
@@ -1,6 +1,16 @@
+import React  from "react";
+
 import Layout from "./view/Layout";
 
+
+
+
+
+
 function App() {
+
+
+
   return (
     <>
     <Layout/>
---
+++ src/view/Notification.js
@@ -0,0 +1,15 @@
+import React, { useEffect, useState } from 'react'
+
+function Notification() {
+   
+  useEffect(()=>{
+
+
+  })
+
+  return (
+ <div></div>
+  )
+}
+
+export default Notification
